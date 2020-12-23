e707f4f9ae3aca0d8e7fdae7bc86f1d91f8c96f7
Bal Jagpal <Balraj_Jagpal@Hotmail.com>
2020-12-23 16:47:11 +0000

    First Factorial

---
+++ FirstFactorial/FirstFactorialApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FirstFactorialApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the Factroial");
+            String response = String.Empty;
+            while (response != "q")
+            {
+                Console.WriteLine("Provide number:");
+                response = Console.ReadLine();
+                long num = 0;
+                if (long.TryParse(response, out num))
+                {
+                    Console.WriteLine($"Factorial is {CalculateFactorial(num)}");
+                }
+            }
+        }
+
+
+        static long CalculateFactorial(long num)
+        {
+            long val = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                val *= i;
+            }
+
+            return val;
+        }
+    }
+}
